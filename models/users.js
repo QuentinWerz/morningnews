91ddc9f174d25839e06298be21416c1024042a52
Quentin Werz <quentin.werz@gmail.com>
2021-04-30 11:15:35 +0200

    MAJ user et schéma article

--- models/users.js
+++ models/users.js
@@ -1,11 +1,27 @@
 var mongoose = require('mongoose')
 
+
+var articlesSchema = mongoose.Schema({
+
+        author: String,
+        title: String,
+        description: String,
+        url: String,
+        urlToImage: String,
+        publishedAt: String,
+        content: String   
+
+})
+
+
 var usersSchema = mongoose.Schema({
     
         name : String,
         email: String,
         password: String,
-        token: String
+        token: String,
+        language: String,
+        wishes: [articlesSchema]
     
 });
 
